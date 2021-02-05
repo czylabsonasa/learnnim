9aa38fe4b9f0989c0f9c05b10aea3383bc48956a
czylabsonasa <czylabson.asa@gmail.com>
2021-02-05 14:05:20 +0100

    progress

---
+++ atCoder/abc/088/a/main.nim
@@ -0,0 +1,6 @@
+import strscans
+
+var n,a: int
+discard scanf(stdin.readAll,"$s$i$s$i",n,a)
+
+echo (if a >= n mod 500: "Yes" else: "No")
---
+++ atCoder/abc/088/b/main.nim
@@ -0,0 +1,14 @@
+import strutils, sequtils, sugar, algorithm
+
+let n = stdin.readLine.parseInt() 
+let a = stdin.readAll.splitWhitespace.
+  map(x=>parseInt(x)).sorted(order=Descending)
+
+var 
+  A = [0,0]
+  j = 0
+for v in a:
+  A[j] += v
+  j = 1 - j
+
+echo A[0]-A[1]
